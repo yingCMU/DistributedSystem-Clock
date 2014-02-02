80227bd0f48fff21d802e96a22ecb677b5838a91
Yuanshuo Cui <aptiva0985@gmail.com>
2014-02-01 23:28:56 -0500

    Remove simulator

--- src/distSysLab1/comm/UserInputThread.java
+++ src/distSysLab1/comm/UserInputThread.java
@@ -3,6 +3,7 @@ package distSysLab1.comm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.UnknownHostException;
 
 import org.apache.log4j.Logger;
 
@@ -87,5 +88,23 @@ public class UserInputThread implements Runnable {
         result[1] = in.readLine();
 
         return result;
+    }
+    
+    public static void main(String[] args) throws IOException {
+        try {
+            String[] arg = init();
+            MessagePasser messagePasser = MessagePasser.getInstance(arg[0], arg[1]);    
+            messagePasser.startListener();
+            messagePasser.startSender();       
+            
+            UserInputThread uiThread = new UserInputThread();
+            Thread thread = new Thread(uiThread); 
+            thread.start();
+            //messagePasser.teminate();         
+                
+        } catch (UnknownHostException e) { 
+            // Auto-generated catch block
+            e.printStackTrace();
+        }       
     }
 }
